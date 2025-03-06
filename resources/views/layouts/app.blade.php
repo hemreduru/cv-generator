d86805fc1b8e22612e3e45e5e6e245055f5602fd
emre <hemreduru@gmail.com>
2025-03-06 23:13:28 +0200

    #2 no translate for browser

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" translate="no">
 @include('layouts.head')
 @stack('head')
 <body class="hold-transition sidebar-mini layout-fixed">
