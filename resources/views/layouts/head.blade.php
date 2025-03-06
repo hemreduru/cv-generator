b5ad2b7f5c0da1f48617395e32c0c4d059997cb2
emre <hemreduru@gmail.com>
2025-03-06 23:46:41 +0200

    minor fixes

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -1,6 +1,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="{{ asset('assets/img/cv-icon.png') }}" type="image/x-icon">
     <title>@yield('title', 'Quick CV')</title>
     <link rel="stylesheet" href="{{ asset('vendor/fontawesome-free/css/all.min.css') }}">
     <link rel="stylesheet" href="{{ asset('vendor/adminlte/dist/css/adminlte.min.css') }}">
