2d7ba757b9720b8c31cefea1dbadc68e23dce4eb
emre <hemreduru@gmail.com>
2025-03-06 16:38:36 +0200

    #1 models, migrations, seeders and factories

---
+++ app/Models/ExperienceResponsible.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ExperienceResponsible extends Model
+{
+    /** @use HasFactory<\Database\Factories\ExperienceResponsibleFactory> */
+    use HasFactory;
+}
